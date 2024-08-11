126da678816215710e3d17f2ada9f2efe86eca25
joaquingutierrez <joaquinguty@gmail.com>
2024-08-11 11:21:17 -0300

    Ahora se verifica el token esta disponible, de ser asi, inicia sesion automaticamente

--- src/context/UserContext/index.jsx
+++ src/context/UserContext/index.jsx
@@ -1,18 +1,51 @@
 import { createContext, useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
 export const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
+
+    const URL = "http://localhost:8080/"
+
     const [user, setUser] = useState({
         name: "",
         rol: ""
     })
 
+    useEffect(() => {
+        const token = Cookies.get("access_token");
+
+        fetch(URL + 'api/validate-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            credentials: 'include'
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.user) {
+                    setUser({
+                        name: data.user.userName,
+                        rol: data.user.rol
+                    });
+                } else {
+                    Cookies.remove('access_token');
+                }
+            })
+            .catch(err => {
+                console.error("Error verifying token", err);
+                Cookies.remove('access_token');
+            });
+
+    }, [])
+
     const openSession = (data) => {
         setUser({
             name: data.userName,
             rol: data.rol
-    })
+        })
     }
 
     const closeSession = () => {
