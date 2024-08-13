6c49eab7cba091d5c4f0ba21d0ff451fd44b2eb1
joaquingutierrez <joaquinguty@gmail.com>
2024-08-13 11:37:23 -0300

    Ahora los empleados muestran cuándo pueden tomar las vacaciones en función del tiempo que llevan

--- src/componentsContainer/EmployeeContainer/index.jsx
+++ src/componentsContainer/EmployeeContainer/index.jsx
@@ -1,26 +1,31 @@
 import "./style.css"
+import { convertDate } from "../../utils/date";
 
-const EmployeeInfoContainer = ({emp}) => {
+const EmployeeInfoContainer = ({ emp }) => {
 
-    const getAvailableVacationDays  = (joiningDate) => {
+    const getAvailableVacationDays = (joiningDate) => {
         const today = new Date();
         joiningDate = new Date(joiningDate)
         const diffMonths = (today.getFullYear() - joiningDate.getFullYear()) * 12 + (today.getMonth() - joiningDate.getMonth());
-    
+
         const maxVacationDays = (diffMonths) => {
-            switch(true) {
+            const nineMonthsDate = new Date(joiningDate)
+            const twelveMonthsDate = new Date(joiningDate)
+            nineMonthsDate.setMonth(joiningDate.getMonth() + 9);
+            twelveMonthsDate.setMonth(joiningDate.getMonth() + 12)
+            switch (true) {
                 case diffMonths < 9:
-                    return 0;
+                    return `Hoy: 0; a partir del ${convertDate(nineMonthsDate)}: 7; a partir del ${convertDate(twelveMonthsDate)}: 14`;
                 case diffMonths >= 9 && diffMonths < 12:
-                    return 7; 
+                    return `Hoy: 7; a partir del ${convertDate(twelveMonthsDate)}: 14`;
                 case diffMonths >= 12:
                     return 14;
                 default:
                     return 0;
             }
+        }
+        return maxVacationDays(diffMonths)
     }
-    return maxVacationDays(diffMonths)
-}
 
     return (
         <div>
